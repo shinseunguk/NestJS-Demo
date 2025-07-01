e696dc3f2c140fb562c8c74e80475b1938dcea5f
신승욱 <ukseung.dev@incross.com>
2025-07-01 16:21:07 +0900

    ✨ 로그인 구현
    https://www.youtube.com/watch?v=u39nqCfjYx4&list=PLeNJ9AVv90q1kEX-bz1r_56imtp5_aJ6q&index=14

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,4 @@
-import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable, UnauthorizedException } from '@nestjs/common';
 import { UserDTO } from './dto/user.dto';
 import { UserService } from './user.service';
 
@@ -16,5 +16,19 @@ export class AuthService {
             throw new HttpException('Username aleady used!', HttpStatus.BAD_REQUEST);
         }
         return await this.userService.save(newUser);
+    }
+
+    async valudateUser(user: UserDTO): Promise<UserDTO | undefined> {
+        let userFind: UserDTO | null = await this.userService.findByFields({ 
+            where: { username: user.username }
+        });
+        if(!userFind) {
+            throw new UnauthorizedException('username을 찾을 수 없습니다.');
+        }
+        if(user.password != userFind.password) {
+            throw new UnauthorizedException('password 오류');
+        }
+
+        return userFind
     }
 }
