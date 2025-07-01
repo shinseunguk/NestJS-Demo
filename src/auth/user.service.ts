c982c37ee3f4edad1f784871aa09a8c8cfa1ea2b
shinseunguk <krdut1@gmail.com>
2025-07-01 18:07:04 +0900

    ✨ bcrypt 적용(로그인, 회원가입)
    https://www.youtube.com/watch?v=LbyfF6ALMo0&list=PLeNJ9AVv90q1kEX-bz1r_56imtp5_aJ6q&index=15

--- src/auth/user.service.ts
+++ src/auth/user.service.ts
@@ -3,6 +3,7 @@ import { InjectRepository } from "@nestjs/typeorm";
 import { Repository } from "typeorm";
 import { User } from "./entity/user.entity";
 import { UserDTO } from "./dto/user.dto";
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class UserService {
@@ -16,6 +17,20 @@ export class UserService {
     }
 
     async save(userDTO: UserDTO): Promise<User> {
-        return await this.userRepository.save(userDTO);
+        const saltRounds = 10;
+        const hashedPassword = await bcrypt.hash(userDTO.password, saltRounds);
+        const userToSave = { ...userDTO, password: hashedPassword };
+        console.log(userToSave);
+        return await this.userRepository.save(userToSave);
+    }
+
+    async transformPassword(user: UserDTO): Promise<void> {
+        const saltRounds = 10;
+
+        user.password = await bcrypt.hash(
+            user.password, saltRounds
+        );
+
+        return Promise.resolve()
     }
 }
