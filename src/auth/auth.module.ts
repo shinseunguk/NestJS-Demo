d819bb6285e128f36c058bca354cc203e9f7ec26
shinseunguk <krdut1@gmail.com>
2025-07-04 09:46:14 +0900

    ✨ JWT 토큰 생성
    https://www.youtube.com/watch?v=H4VS-Osylvo&list=PLeNJ9AVv90q1kEX-bz1r_56imtp5_aJ6q&index=16

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -4,9 +4,16 @@ import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { UserService } from './user.service';
 import { User } from './entity/user.entity';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User])],
+  imports: [
+    TypeOrmModule.forFeature([User]),
+    JwtModule.register({
+      secret: 'SECRET_KEY',
+      signOptions: {expiresIn: '300s'},
+    })
+  ],
   controllers: [AuthController],
   providers: [AuthService, UserService]
 })
